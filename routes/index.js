5134a512a64096ff15f9a4489d33ce7335d1a8ab
Sheharyar Naseem <sherry@rocketbots.io>
2022-02-07 16:54:01 +0800

    feat: updates send message handler for clicksend feat: handles inbound messages from clicksend to respond.io

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,23 @@
 /* eslint-disable new-cap */
 const express = require('express');
+const axios = require('axios')
 
 /**
- * TeleSign (SMS Provider) configuration
+ * ClickSend: https://clicksend.com (SMS Provider)
  */
-const TeleSignSDK = require('telesignsdk');
-const telesign = new TeleSignSDK(
-    'FFFFFFFF-EEEE-DDDD-1234-AB1234567890', // customerId
-    'EXAMPLE----TE8sTgg45yusumoN4BYsBVkh+yRJ5czgsnCehZaOYldPJdmFh6NeX8kunZ2zU1YWaUw/0wV6xfw==', // apiKey
-    'https://rest-api.telesign.com', // restEndpoint
-);
+const api = require('./api.js');
 
 /**
  * Respond.io custom channel API Token
  */
 const CHANNEL_API_TOKEN = '<API Token>';
 
-
 const router = express.Router();
 
+
+/**
+ * Example for handling outbound messages FROM respond.io TO ClickSend: https://clicksend.com
+ */
 router.post('/message', (req, res) => {
   /**
      * Authentication
@@ -31,18 +30,87 @@ router.post('/message', (req, res) => {
   }
 
   /**
-     * Sending SMS using third party sms provider i.e (TeleSign)
-     */
+   * Sending SMS using third party sms provider i.e (ClickSend: https://clicksend.com)
+   */
+  const smsMessage = new api.SmsMessage();
+
+
   const phoneNumber = req.body.contactId;
   const message = req.body.message.text;
 
-  telesign.sms.message((e, r) => {
-            e ? res.status(400).send({'error': {'message': e.res.message}}) :
-                res.send({mId: r.reference_id});
-  },
-  phoneNumber,
-  message,
-  );
+  smsMessage.source = "sdk";
+  smsMessage.to = phoneNumber;
+  smsMessage.body = message;
+
+  const smsCollection = new api.SmsMessageCollection();
+
+  smsCollection.messages = [smsMessage];
+
+  smsApi.smsSendPost(smsCollection)
+      .then(r => {
+        res.send({mId: r.data.messages[0].message_id})
+      })
+      .catch(e => {
+        switch (e.response.status) {
+          case 401:
+            res.status(400)
+                .send({'error': {'message': '401: UNAUTHORIZED'}})
+            break;
+          default:
+            res.status(400)
+                .send({'error': {'message': `err: status_code #${e.response.status} `}})
+        }
+      });
+});
+
+
+/**
+ * Example for handling inbound messages FROM ClickSend: https://clicksend.com TO respond.io
+ */
+router.post('/clicksend/push_message', (req, res) => {
+
+  /**
+   * Reference for push message
+   * https://developers.clicksend.com/docs/rest/v3/#view-inbound-sms
+   *
+   * We will be preparing the data for the webhook
+   * We will be sending generated data to the respond.io
+   */
+  const messageId = res.data.message_id;
+  const from = res.data.from;
+  const messageBody = res.data.body;
+  // converts ClickSend timestamp to milliseconds
+  const timestamp = (res.data.timestamp * 1000);
+
+  const data = {
+    channelId: "gfd8g7fd89dgfd",
+    contactId: from,
+    events: [
+      {
+        type: "message",
+        mId: messageId,
+        timestamp,
+        message: {
+          type: "text",
+          text: messageBody
+        }
+      }
+    ]
+  }
+
+
+  // Calls respond.io webhook
+  axios({
+    method: 'post',
+    url: 'https://app.respond.io/custom/webhook',
+    headers: {
+      'authorization': `Bearer ${CHANNEL_API_TOKEN}`,
+      'content-type': 'application/json',
+      'cache-control': 'no-cache'
+    },
+    data
+  });
+
 });
 
 module.exports = router;
